a26a08b5c7a6c73fd96fc472ea83f27545caee5e
valerenegoh <valerene.goh@thoughtworks.com>
2021-06-19 14:24:29 +0800

    [Valerene] removed counting repetition in NumberCruncher

--- src/main/java/com/thoughtworks/twu/NumberCruncher.java
+++ src/main/java/com/thoughtworks/twu/NumberCruncher.java
@@ -4,40 +4,38 @@ import java.util.List;
 
 public class NumberCruncher {
     private final List<Integer> numbers;
+    private int even;
+    private int odd;
+    private int positive;
+    private int negative;
 
     public NumberCruncher(List<Integer> numbers) {
         this.numbers = numbers;
+        countAll();
+    }
+
+    private void countAll() {
+        for (int number : numbers) {
+            if (number % 2 == 0) even++;
+            if (number % 2 == 1) odd++;
+            if (number >= 0) positive++;
+            if (number < 0) negative++;
+        }
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return even;
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return odd;
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
+        return positive;
     }
 
     public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
-        }
-        return count;
+        return negative;
     }
 }
